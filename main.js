669e70dea5a570b9df989dc996f0bd1dce19a69b
Monem Dakheel <monem.dakheel@buniq.it>
2023-01-02 15:44:27 +0100

    updater implementation

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 const { app, BrowserWindow } = require('electron');
+const log = require('electron-log');
+const {autoUpdater} = require("electron-updater");
 const path = require('path')
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -15,7 +43,10 @@ const createWindow = () => {
 
   app.whenReady().then(() => {
     createWindow();
+    autoUpdater.checkForUpdatesAndNotify();
     app.on('activate', () => {
       if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
   });
+
+  
